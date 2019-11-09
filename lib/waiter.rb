8b707df66fca6e5faaff6a3663e715d96f9f6938
Justin Phillips <justinphil02@yahoo.com>
2019-11-09 02:37:24 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,17 +1,17 @@
 class Waiter
-  
+
   attr_reader :name, :yrs_experience
   @@all = []
 
   def initialize(name, exp)
     @name = name
     @yrs_experience = exp
-    @@all << self 
+    @@all << self
   end
 
-  def self.all 
+  def self.all
     @@all
-  end 
+  end
 
 
 end #end of class
