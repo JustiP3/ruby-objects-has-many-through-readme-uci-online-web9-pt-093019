e48f04edefb9335482115b8ac59e1aca2ba703ad
Justin Phillips <justinphil02@yahoo.com>
2019-11-09 02:51:56 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -3,11 +3,16 @@ class Waiter
   attr_reader :name, :yrs_experience
   @@all = []
 
+
   def initialize(name, exp)
     @name = name
     @yrs_experience = exp
     @@all << self
   end
+
+  def new_meal(cutomer, total, tip)
+    Meal.new(self, customer, total, tip)
+  end 
 
   def self.all
     @@all
