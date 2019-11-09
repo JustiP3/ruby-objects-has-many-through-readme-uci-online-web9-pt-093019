137f70c82458c3f7b70b31e808f21f15c1985a14
Justin Phillips <justinphil02@yahoo.com>
2019-11-09 02:19:13 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,16 @@
 class Waiter
+attr_reader :name, :yrs_experience
+@@all = []
 
+def initialize(name, exp)
+@name = name
+@yrs_experience = exp
+@@all << self 
 end
+
+def self.all 
+  @@all
+end 
+
+
+end #end of class
