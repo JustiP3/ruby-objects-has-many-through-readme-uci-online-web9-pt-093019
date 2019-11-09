c989d40c06f6e3b506e52b66c17e6a34dd050cbb
Justin Phillips <justinphil02@yahoo.com>
2019-11-09 02:36:52 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -2,16 +2,16 @@ class Meal
   attr_reader :waiter, :customer, :total, :tip
   @@all = []
 
-def initialize(waiter,customer,total,tip)
-@waiter = waiter
-@customer = customer
-@total = total
-@tip = tip
-@@all << self
-end
+  def initialize(waiter,customer,total,tip)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all << self
+  end
 
-def self.all
-  @@all
-end
+  def self.all
+    @@all
+  end
 
 end
